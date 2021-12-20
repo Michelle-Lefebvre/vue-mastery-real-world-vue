6c99a41e61a3df719a2f775c52b96d5ca9e95dc0
Michelle <michelle.lefebvre@valtech.com>
2021-12-20 10:57:43 -0500

    started new course Mastering Vuex, State & Getters, Mapping to Computed, also renamed router & store files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
+import router from './router/router'
+import store from './store/store'
 
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
@@ -44,6 +44,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  store, // injected vuex into all components
   render: (h) => h(App),
 }).$mount('#app')
